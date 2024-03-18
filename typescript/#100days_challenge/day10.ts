0fccff1916210e44b1b8e6801a5a3a9e02219396
404MW <404mwaqas@gmail.com>
2024-03-19 00:25:18 +0500

    did day10

---
+++ typescript/#100days_challenge/day10.ts
@@ -0,0 +1,39 @@
+// else it practice
+
+let agE: number = 35
+if (agE <= 4 ) {
+    console.log("Look, a  baby!!!");
+} else if(agE <= 12) {
+    console.log("Look, a kiddo!!!");
+} else if (agE <= 20) {
+    console.log("Look, a teenager!!!");
+} else if (agE <= 25) {
+    console.log("Look, an adult!!!");
+} else if (agE > 25 ) {
+    console.log("Look, an elder!!!");
+}
+
+
+// array x if-else
+
+let fruits: string[] = ["Mango", "Kiwi", "Strawberry", "Melon"]
+if (fruits.includes("Melon")) {
+    console.log("You really like Melons");
+} if (fruits.includes("Mango")) {
+    console.log("You really like Mangoes");
+}
+
+
+
+
+//
+
+let usernames: string[] = ["M.W.", "Ebad", "Abdullah", "Jawwad", "Aslam"];
+
+usernames.forEach(username => {
+    if (username === "M.W.") {
+        console.log("Hello Admin, would you like to missuse your power (for fun)");
+    } else {
+        console.log("Hello "+ username +", Its nice to see you again.");
+    }
+});
