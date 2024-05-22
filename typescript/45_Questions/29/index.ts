14c929705a667833beba44a41918740249d964f4
404MW <404mwaqas@gmail.com>
2024-05-22 21:36:17 +0500

    30/45 done

---
+++ typescript/45_Questions/29/index.ts
@@ -0,0 +1,29 @@
+/*
+Favorite Fruit: Make a array of your favorite fruits, 
+and then write a series of independent if statements that check for certain fruits in your array.
+
+• Make a array of your three favorite fruits and call it favorite_fruits.
+• Write five if statements. Each should check whether a certain kind of fruit is in your array.
+  If the fruit is in your array, the if block should print a statement, such as You really like bananas! */
+
+let task29 = ["Apple", "Mango", "Cherry"]
+
+if (task29.includes("Mango")){
+    console.log("You really like Mangoes!");
+};
+
+if (task29.includes("Pineapple")){
+    console.log("You really like Pineapples!");
+};
+
+if (task29.includes("Melon")){
+    console.log("You really like Melons!");
+};
+
+if (task29.includes("Apple")){
+    console.log("You really like Apples!");
+};
+
+if (task29.includes("Kiwi")){
+    console.log("You really like Kiwies!");
+};
