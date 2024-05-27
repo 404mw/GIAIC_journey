b8fae0d09664119085b6c5b8cde217bbcb848b2e
404MW <404mwaqas@gmail.com>
2024-05-27 23:37:03 +0500

    33/45 done

---
+++ typescript/45_Questions/31/index.ts
@@ -0,0 +1,14 @@
+/*
+No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+
+• If the list is empty, print the message We need to find some users!
+• Remove all of the usernames from your array, and make sure the correct message is printed. */
+
+
+let task31 = [1, 2, 3, 4, 5, 6]
+
+task31.length = 0
+
+if (task31.length == 0){
+    console.log(`We need to find some users!`);
+} else {};
