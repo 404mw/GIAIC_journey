4f0a25497d02277d15f002fd03ed8aa733041924
404MW <404mwaqas@gmail.com>
2024-05-28 23:20:25 +0500

    39/45 done

---
+++ typescript/45_Questions/37/index.js
@@ -0,0 +1,9 @@
+"use strict";
+/*
+Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript.
+Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+*/
+function make_shirt2(size = "large", message = "I love TypeScript") {
+    console.log(`Make a ${size} sized shirt with a message "${message}" printed on it`);
+}
+make_shirt2("medium");
